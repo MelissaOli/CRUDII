13f149d4ddd235db3b437da8c5d2b8f124e4907c
MelissaOli <melissaparecida15@gmail.com>
2022-08-19 17:35:47 -0300

    first commit

---
+++ resources/views/editar.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Produtos</title>
+    </head>
+    <body>
+        <h1>Produtos</h1>
+		<form action="/editar-produto/{{ $produto-> id }}" method="POST">
+			@csrf
+			<label for="lblNome" >Nome:</label>
+			<input type="text" name="nome"/>
+            <input type="text" name="nome" value="{{ $produto->nome }}">
+			<br><br>
+			
+			<label for="lblValor" >Valor:</label>
+			<input type="text" name="valor"/>
+            <input type="text" name="valor" value="{{ $produto->valor }}">
+			<br><br>
+			
+			<label for="lblQuantidade" >Estoque:</label>
+			<input type="text" name="estoque"/>
+            <input type="text" name="estoque" value="{{ $produto->estoque }}">
+			<br><br>
+			<button>Cadastrar</button>
+    	</form>
+    </body>
+</html>
